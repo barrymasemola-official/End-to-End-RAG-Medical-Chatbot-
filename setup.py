99e03d21e5efe12f502c371c92c92e28b64ad822
Tumisho Masemola <Barrymasemola@gmail.com>
2026-02-23 20:46:23 +0200

    Creation of setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages , setup
+
+setup(
+
+    name="Medical chatbot",
+    version= "0.1.0",
+    author="Tumisho Masemola",
+    author_email="Barrymasemola@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+
+)
