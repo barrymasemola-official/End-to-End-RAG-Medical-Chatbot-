1c0c120f965f2968bbb0b91aac68f7b3ddf04f33
Tumisho Masemola <Barrymasemola@gmail.com>
2026-02-23 19:48:56 +0200

    Folder structure added

---
+++ template.sh
@@ -0,0 +1,12 @@
+mkdir -p src
+mkdir -p research
+touch src/--init--.py
+touch src/helper.py
+touch src/prom.py
+touch .enc
+touch setup.py
+touch research/trial.ipynb
+touch requirements.txt
+
+
+echo "Directory and files successfully created"
